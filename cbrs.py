58dd2add27cb61f8d82dd2a2a976cdc5ed463cc6
Paulo Santos <pauloroberto.santos@edge.ufal.br>
2024-03-31 05:40:20 -0300

    feat: Updates code to a council of algorithms.
    Now most of the available scipy.spatial.distance algorithms vote for a prognostic, leading to a more understandable and reliable decision.
    
    Signed-off-by: Paulo Santos <pauloxrms@gmail.com>

--- cbrs.py
+++ cbrs.py
@@ -8,8 +8,27 @@ import tkinter as tk
 from tkinter import ttk
 
 reported_symptoms = []
-inferences_list = []
-inf_checkboxes = []
+council_history = []
+last_inference = tuple()
+
+metrics = sorted([
+    "correlation",
+    "jaccard",
+    "cosine",
+    "hamming",
+    "jensenshannon",
+    "canberra",
+    "braycurtis",
+    "matching",
+    "dice",
+    "rogerstanimoto",
+    "russellrao",
+    "sokalmichener",
+    "sokalsneath",
+    "sqeuclidean",
+    "cityblock",
+    "euclidean"
+])
 
 
 def get_data() -> tuple[tuple[tuple[str], tuple[str]], DataFrame]:
@@ -18,7 +37,9 @@ def get_data() -> tuple[tuple[tuple[str], tuple[str]], DataFrame]:
     symptoms = tuple(db.columns[:-1])
     prognostics = tuple(db['prognóstico'].unique())
 
-    print(f"No dataset, há {len(symptoms)} sintomas para avaliar e {len(prognostics)} prognósticos possíveis.\n")
+    print(
+        f"No dataset, há {db.last_valid_index()} casos, {len(symptoms)} sintomas para avaliar e "
+        f"{len(prognostics)} prognósticos possíveis.\n")
 
     print("\nPrognósticos avaliáveis:")
     for p in sorted(prognostics):
@@ -34,7 +55,8 @@ def commit_knowledge(new_inference: pd.DataFrame, raw_inference: list):
     except FileNotFoundError:
         library = new_inference
 
-    inferences_list.append((raw_inference, new_inference))
+    global last_inference
+    last_inference = (raw_inference, new_inference)
 
     try:
         os.mkdir("output")
@@ -44,47 +66,53 @@ def commit_knowledge(new_inference: pd.DataFrame, raw_inference: list):
     library.to_csv('output/library.csv', index=False)
 
 
-def distance_similarity(distances, target_index) -> float:
-    sorted_indexes = np.argsort(distances)
-    return 1 - (distances[target_index] / distances[sorted_indexes[-1]])
+def council_decision(council_votes: list[str]) -> dict[str, float]:
+    vote_count = {}
+    for vote in set(council_votes):
+        vote_count[vote] = council_votes.count(vote)
+    total = sum(vote_count.values())
+    return {vote: (count / total) * 100 for vote, count in vote_count.items()}
 
 
-def make_prognostic(knowledge: pd.DataFrame, symptoms: list[str]) -> (str, str):
-    base = knowledge.iloc[:, range(knowledge.shape[1] - 1)]
-    symptom_presences = {f"{s}": 0 for s in knowledge.columns[:-1]}
+def format_decision(vote_count: list[tuple[str, float]]) -> str:
+    total = sum([i[1] for i in vote_count])
+    text = ""
+    for vote, count in vote_count:
+        porc = (count / total) * 100
+        text += f" • {vote} ({porc:.2f}%)\n"
+    return text
 
-    print(f"\n\n>Caso: {' + '.join(symptoms)}")
+
+def make_prognostic(knowledge_base: pd.DataFrame, symptoms: list[str]) -> (str, str):
+    base = knowledge_base.iloc[:, range(knowledge_base.shape[1] - 1)]
+    symptom_presences = {f"{s}": 0 for s in knowledge_base.columns[:-1]}
+
     for symptom in symptoms:
         symptom_presences[symptom] = 1
 
-    metrics = ["correlation", "jaccard"]
     distances = {metric: np.zeros(base.shape[0]) for metric in metrics}
 
     one_hot_symptoms = list(symptom_presences.values())
 
-    best_inf = "NULL"
-    best_ratio = 0.0
     estimated_prognostics = {}
     for metric in metrics:
         distances[metric] = distance.cdist(np.array([one_hot_symptoms]), base, metric)
 
         min_metric_index = np.argsort(distances[metric][0])[0]
-        inference = knowledge.iloc[min_metric_index, -1]
-        closeness = distance_similarity(distances[metric][0], min_metric_index)
-        estimated_prognostics[metric] = {"inference": inference, "closeness": closeness}
+        estimated_prognostics[metric] = knowledge_base.iloc[min_metric_index, -1]
 
-        if closeness > best_ratio:
-            best_ratio = closeness
-            best_inf = inference
+    inference_voting_report = council_decision(list(estimated_prognostics.values()))
+    council_history.append(inference_voting_report)
 
-        print(f"\tPor {metric} -> {inference} ({closeness * 100:.2f})")
+    most_voted_inference = max(inference_voting_report.items(), key=lambda x: x[1])[0]
 
-    case = np.append(one_hot_symptoms, best_inf)
+    one_hot_symptoms = [-1 if i == 0 else 1 for i in one_hot_symptoms]
+    case = np.append(one_hot_symptoms, most_voted_inference)
 
     # Acumula o novo conhecimento.
-    case = pd.DataFrame(case, list(knowledge.columns)).transpose()
+    case = pd.DataFrame(case, list(knowledge_base.columns)).transpose()
     case.iloc[-1, :-1] = [int(i) for i in case.iloc[-1, :-1]]
-    commit_knowledge(case, [symptoms.copy(), best_inf])
+    commit_knowledge(case, [symptoms.copy(), most_voted_inference])
 
     return estimated_prognostics
 
@@ -100,23 +128,29 @@ def add_symptom_cb():
 
         make_prognostic_button.config(state=tk.NORMAL)
         clean_selections_button.config(state=tk.NORMAL)
-    else:
-        make_prognostic_button.config(state=tk.DISABLED)
-        clean_selections_button.config(state=tk.DISABLED)
+        undo_selections_button.config(state=tk.NORMAL)
 
 
 def make_prognostic_cb():
-    result_label.config(text="> Analisando... ⌛")
+    result_label.config(text="⌛ Analisando...")
+    tabs.select(tab_id=1)
     result_label.update()
     prg_dict = make_prognostic(knowledge, reported_symptoms)
 
-    prog_str = ""
-    for m, pr in prg_dict.items():
-        prog_str += f"    Por {m.capitalize()} → {pr['inference']} ({pr['closeness'] * 100:.2f}%) ou \n"
+    most_common_inf = sorted(council_history[-1].items(), key=lambda x: x[1], reverse=True)
 
-    result_label.config(text=f"> Para o caso avaliado, o prognóstico é :\n"
-                             f"{prog_str[:-4]}")
+    prog_str = "Opinião do conselho médico:\n"
+    for m, prog in prg_dict.items():
+        prog_str += f" • Dr. {m.capitalize() + ':': <15} {prog}\n"
+    prog_str += "\nConclusões:\n" + format_decision(most_common_inf)
+
+    symp_str = '\n'.join([f" • {i.capitalize()}" for i in sorted(reported_symptoms)])
+    print(f"\n===\nSintomas:\n{symp_str}\n\n{prog_str}\n===\n")
+
     make_prognostic_button.config(state=tk.DISABLED)
+    save_knowledge_button.config(state=tk.NORMAL)
+    result_label.config(text=prog_str)
+    result_label.update()
 
 
 def clean_selection_cb():
@@ -126,38 +160,33 @@ def clean_selection_cb():
     symptoms_list_box.config(state=tk.DISABLED)
     make_prognostic_button.config(state=tk.DISABLED)
     clean_selections_button.config(state=tk.DISABLED)
+    undo_selections_button.config(state=tk.DISABLED)
+    result_label.config(text="")
+
+
+def undo_selection_cb():
+    reported_symptoms.pop()
+    symptoms_list_box.config(state=tk.NORMAL)
+    symptoms_list_box.delete("1.0", tk.END)
+    for symptom in reported_symptoms:
+        symptoms_list_box.insert(tk.END, f"• {symptom.capitalize()}\n")
+    symptoms_list_box.config(state=tk.DISABLED)
+    if len(reported_symptoms) == 0:
+        make_prognostic_button.config(state=tk.DISABLED)
+        clean_selections_button.config(state=tk.DISABLED)
+        undo_selections_button.config(state=tk.DISABLED)
+    else:
+        make_prognostic_button.config(state=tk.NORMAL)
     result_label.config(text="")
 
 
 def save_coherent_knowledge_cb():
-    validated_knowledge = pd.DataFrame()
     database = pd.read_csv('input/database.csv')
 
-    for i, inf in enumerate(inferences_list):
-        if inf_checkboxes[i].get():
-            validated_knowledge = pd.concat([validated_knowledge, inf[1]], ignore_index=True)
+    database = pd.concat([database, last_inference[1]], ignore_index=True)
+    database.to_csv('input/database.csv', index=False)
 
-    if not validated_knowledge.empty:
-        database = pd.concat([database, validated_knowledge], ignore_index=True)
-        database.to_csv('input/database.csv', index=False)
-
-    inferences_list.clear()
-    inf_checkboxes.clear()
-    update_history_cb()
-
-
-def update_history_cb():
-    for widget in history_frame.winfo_children():
-        widget.destroy()
-
-    for inf in inferences_list:
-        check_var = tk.BooleanVar()
-        check_frame = ttk.Frame(history_frame)
-        check_button = ttk.Checkbutton(check_frame, text=f"{' + '.join(inf[0][0])} → {inf[0][1]}",
-                                       variable=check_var)
-        check_button.pack(side=tk.LEFT)
-        check_frame.pack(fill=tk.BOTH)
-        inf_checkboxes.append(check_var)
+    save_knowledge_button.config(state=tk.DISABLED)
 
 
 if __name__ == '__main__':
@@ -167,8 +196,9 @@ if __name__ == '__main__':
     root.title('Sistema de Prognóstico de Doenças')
     style = ttk.Style()
     style.theme_use("clam")
+    style.configure('tabs.TNotebook.Tab', font="TkFixedFont")
 
-    tabs = ttk.Notebook(root)
+    tabs = ttk.Notebook(root, style="tabs.TNotebook")
     tabs.pack(fill="both", expand=True)
 
     main_frame = ttk.Frame(tabs)
@@ -178,7 +208,7 @@ if __name__ == '__main__':
     selected_symptom_var.set("Selecione um Sintoma")
     selected_symptom_var.trace_add("write", lambda x, y, z: add_symptom_button.config(state=tk.NORMAL))
     symptoms_list_combobox = ttk.Combobox(selection_frame, textvariable=selected_symptom_var, width=40,
-                                          values=list(map(str.capitalize, sorted(all_symptoms))))
+                                          values=sorted([s.capitalize() for s in all_symptoms]))
     symptoms_list_combobox.bind("<Button-1>", lambda e:
                                 symptoms_list_combobox.focus() or symptoms_list_combobox.event_generate('<Down>'))
     symptoms_list_combobox.bind("<FocusOut>", lambda e: symptoms_list_combobox.selection_clear())
@@ -195,24 +225,29 @@ if __name__ == '__main__':
     prognostic_frame = ttk.Frame(main_frame)
     make_prognostic_button = ttk.Button(prognostic_frame, text="Realizar Prognóstico", state=tk.DISABLED,
                                         command=make_prognostic_cb)
-    make_prognostic_button.pack(pady=10, padx=30, side=tk.RIGHT)
+    make_prognostic_button.pack(pady=10, padx=5, side=tk.RIGHT)
 
-    clean_selections_button = ttk.Button(prognostic_frame, text="Limpar Seleção", state=tk.DISABLED,
+    clean_selections_button = ttk.Button(prognostic_frame, text="Limpar", state=tk.DISABLED,
                                          command=clean_selection_cb)
-    clean_selections_button.pack(pady=10, padx=30, side=tk.LEFT)
+    clean_selections_button.pack(pady=10, padx=5, side=tk.LEFT)
+
+    undo_selections_button = ttk.Button(prognostic_frame, text="Desfazer", state=tk.DISABLED,
+                                        command=undo_selection_cb)
+    undo_selections_button.pack(pady=10, padx=5, side=tk.LEFT)
+
     prognostic_frame.pack()
 
-    result_label = ttk.Label(main_frame, text="")
-    result_label.pack(padx=10, pady=1)
     tabs.add(main_frame, text="🧩 Inferência")
 
     knowledge_frame = ttk.Frame(tabs)
-    history_frame = ttk.Frame(knowledge_frame)
-    history_frame.pack()
-    ttk.Label(knowledge_frame, text="Selecione as inferências coerentes.").pack()
-    knowledge_frame.bind("<FocusIn>", lambda e: update_history_cb())
+    ttk.Label(knowledge_frame, text="").pack()
+    result_label = ttk.Label(knowledge_frame, text="", font="TkFixedFont")
+    result_label.pack(padx=5, pady=0)
 
-    ttk.Button(knowledge_frame, text="Salvar Selecionadas", command=save_coherent_knowledge_cb).pack(padx=10, pady=10)
+    save_knowledge_button = ttk.Button(knowledge_frame, text="Salvar Prognóstico", command=save_coherent_knowledge_cb)
+    save_knowledge_button.pack(padx=10, pady=10)
+    save_knowledge_button.config(state=tk.DISABLED)
+    ttk.Label(knowledge_frame, text="").pack()
 
     tabs.add(knowledge_frame, text="🧠 Verificação e Conhecimento")
 
